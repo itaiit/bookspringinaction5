f7c2ccd6e109acb054e9ef31dd629a1da0ad9134
itaiit <2264044269@qq.com>
2022-08-25 21:20:45 +0800

    添加springsecurity，基于jdbc的认证

--- taco-cloud/src/main/java/io/itaiit/config/SecurityConfig.java
+++ taco-cloud/src/main/java/io/itaiit/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package io.itaiit.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * @author itaiit
@@ -13,11 +17,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("buzz")
-                .password("{noop}12345")
-                .authorities("ROLE_USER");
+//        auth.inMemoryAuthentication()
+//                .withUser("buzz")
+//                .password("{noop}12345")
+//                .authorities("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
     }
 }
