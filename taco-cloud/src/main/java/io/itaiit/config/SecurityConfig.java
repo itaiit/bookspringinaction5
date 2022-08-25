85babeb641ce7e7ff3add95714c886513db07bad
itaiit <2264044269@qq.com>
2022-08-25 21:04:07 +0800

    添加springsecurity，基于内存用户的认证

---
+++ taco-cloud/src/main/java/io/itaiit/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package io.itaiit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author itaiit
+ * @date 2022/8/25 20:41
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("buzz")
+                .password("{noop}12345")
+                .authorities("ROLE_USER");
+    }
+}
